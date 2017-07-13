c95deb67bf06c09534b354aaa325d771604aeead
lengyu_ <315172009@qq.com>
2017-07-13 16:05:22 +0800

    Initial commit
    ```

---
+++ src/main/java/com/github/lengyu/wechat/WxMpDemoApplication.java
@@ -0,0 +1,23 @@
+package com.github.lengyu.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * @author Binary Wang(https://github.com/binarywang)
+ */
+@SpringBootApplication
+public class WxMpDemoApplication extends SpringBootServletInitializer{
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxMpDemoApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(WxMpDemoApplication.class);
+    }
+}
